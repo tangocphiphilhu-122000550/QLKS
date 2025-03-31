e88c1d698bc8aef7f8a6498dc132ba732a5013bb
VoDaiNhan <142654553+VoDaiNhan@users.noreply.github.com>
2025-03-31 21:36:02 +0700

    Add files via upload

---
+++ QLKS/Models/PhongVM.cs
@@ -0,0 +1,18 @@
+﻿using QLKS.Data;
+
+namespace QLKS.Models
+{
+    public class PhongVM
+    {
+
+        public int? MaLoaiPhong { get; set; }
+
+        public string? TenPhong { get; set; }
+
+        public string? TrangThai { get; set; }
+    }
+    public class PhongMD : PhongVM
+    {
+        public string MaPhong { get; set; } = null!;
+    }
+}
