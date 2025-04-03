d43377fffcfc8af9a7416d3a60ae506d55ce544a
VoDaiNhan <142654553+VoDaiNhan@users.noreply.github.com>
2025-04-03 20:49:08 +0700

    Update Qlks1Context.cs

--- QLKS/Data/Qlks1Context.cs
+++ QLKS/Data/Qlks1Context.cs
@@ -39,7 +39,7 @@ public partial class Qlks1Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=PHIPHI\\PHIPHI;Initial Catalog=QLKS1;Integrated Security=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=118.69.126.49;Database=data_QLKS_112_Nhom3;User ID=user_112_nhom3;Password=123456789;Encrypt=False;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
